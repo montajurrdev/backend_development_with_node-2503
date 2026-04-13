5d0efb033fce0ba3280c8590842bf442303aabc3
Montajur Rahman <montajurrdev@gmail.com>
2026-04-14 04:30:35 +0600

    remove env file and ignore it

---
+++ 01__Raw_Node/04___http_server_p-2/httpServer.js
@@ -0,0 +1,67 @@
+// http module provides the low-level interface for transferring data
+// enables the creation of web servers 
+// and the ability to make HTTP requests to other servers
+
+// Server Creation: provides the http.createServer() method to create an http server instance
+// that listens for incoming requests on a specified port and host
+
+// Request and Response Handling: it offers req and res objects within the request listener callback function
+// req object: instance of http.IncomingMessage, req.url(access req url), req.method, req.headers
+// res object: instance of http.ServerResponse. used to build and send res 
+// using methods like res.writeHead(), res.setHeader(), res.write(), res.end()
+
+// Client Requests: Make http req to external APIs or servers using http.request() or simplified http.get() method
+
+// Streaming Support: request and response treated as streams, allowing efficient handling of large data payloads
+
+// Headers and Status Codes: provide methods like res.writeHead() and res.setHeader() to manage HTTP headers
+// and res.statusCode for code status (like 200 OK, 404 Not Found)
+
+// import { log } from 'node:console';
+// import http from 'node:http'
+
+// const hostname = 'localhost';
+// const port = 3000;
+
+// const server = http.createServer((req, res)=>{
+// res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/plain');
+
+// res.end("Hello World\n");
+
+// })
+
+// server.listen(port, hostname, ()=>{
+//     log(`Server ruuning at http://${hostname}:${port}/`)
+// })
+
+
+import http from "node:http"
+
+
+// define two constant, the host and port
+
+const host = 'localhost';  // 127.0.0.1   
+const port = 3000;  // endpoint or door
+
+// request listener function - handle HTTP request and HTTP response
+//  must have two arguments request object, and response object
+
+const requestListener = function(req, res){
+    res.writeHead(200);    // status code
+    res.end("My first server!");  
+
+}
+
+// create server object via http module createServer() function.
+const server = http.createServer(requestListener)
+
+// bind server to a network address for connections
+server.listen(port, host, ()=>{
+console.log(`Server is running on http://${host}:${port}`);
+
+})
+// port, host, and callback function are optional
+
+
+
