3f4a6e7c9df2c5bc16d85e17caa558ffe9d30c09
Montajur Rahman <montajurrdev@gmail.com>
2026-04-14 17:16:54 +0600

    crud done

--- 02__Express/02___todo-using-mongoDB/index.js
+++ 02__Express/02___todo-using-mongoDB/index.js
@@ -9,13 +9,47 @@ app.use(e.json())
 app.use(e.urlencoded())
 
 
+app.get("/tasks/:taskID", async (req, res) => {
+    const {taskID} = req.params;
+  const singleTask = await Task.findById(taskID);
+
+  return res.status(200).json({ data: singleTask });
+});
+
+
+app.get("/tasks/", async (req, res) => {
+const allTasks = await Task.find()
+ 
+  return res.status(200).json({ data: allTasks });
+});
+
 app.post("/tasks/create/", async (req, res)=>{
     const {title, details} = req.body;
     const createdTask = await Task.create({title, details})
-    return res.json({data: createdTask})
+    return res.status(201).json({data: createdTask})
     
 })
 
+app.delete("/tasks/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+   const singleTask = await Task.findByIdAndDelete(taskId);
+//   return res.json({ data: singleTask });
+  return res.status(204).end()   // status send korle ar kichu jay na
+});
+
+
+app.patch("/tasks/:taskId", async (req, res) => {
+    const {taskId} = req.params;
+  const { title, details } = req.body;
+  const updateTask = await Task.findByIdAndUpdate(taskId, {
+    title,
+    details,
+  },
+{
+    new: true,
+})
+  return res.status(201).json({ data: updateTask });
+});
 
 app.get('/', (req, res)=>{
     res.send("Welcome");
